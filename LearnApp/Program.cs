7f6e6821611553a000a511ea704c34b49527a2b5
NotAloneAgain <maoffru@gmail.com>
2024-03-28 20:41:35 +0300

    Более красивое отображение

--- LearnApp/Program.cs
+++ LearnApp/Program.cs
@@ -5,13 +5,47 @@ namespace LearnApp
 {
     public class Program
     {
+        private static readonly List<ConsoleColor> _colors;
+        private static readonly Random _random;
+
+        static Program()
+        {
+            _random = new();
+            _colors = Enum.GetValues(typeof(ConsoleColor)).Cast<ConsoleColor>().ToList();
+        }
+
         private static void Main()
         {
+            Console.ForegroundColor = _colors.ElementAt(_random.Next(0, _colors.Count()));
+
+            Console.WriteLine("Тесты конструкторов:");
+
             TestConstructors();
+
+            UpdateConsole();
+
+            Console.WriteLine("Тесты методов:");
 
             TestMethods();
 
-            Console.WriteLine("Привет, мир!");
+            UpdateConsole();
+
+            Console.WriteLine("Вывод и ввод:");
+
+            Console.WriteLine("Привет, мир!\nВведи что-либо:");
+
+            Console.WriteLine("Ты ввел: {0}", Console.ReadLine());
+
+            Console.ReadLine();
+        }
+
+        private static void UpdateConsole()
+        {
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.ForegroundColor = _colors.ElementAt(_random.Next(0, _colors.Count));
         }
 
         private static void TestConstructors()
