a74ea9205400e6fc6a54d96c26814637505c1b23
NotAloneAgain <maoffru@gmail.com>
2024-03-28 20:00:18 +0300

    Добавить статичный класс

---
+++ LearnApp/Beginning/3. Type Modifiers/Static/StaticClass.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnApp.Beginning.TypeModifiers.Static
+{
+    // Он не может иметь экземпляры.
+    // Он не может иметь нестатические члены.
+    // Обращение всегда идет только через название класса.
+    public static class StaticClass
+    {
+    }
+}
